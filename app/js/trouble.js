308314e25a9bf2c9374d1e8ef2f903cd5b0dba7a
dianezev <dianezev@comcast.net>
2015-04-07 12:27:35 -0700

    jshint corrections
    made edits to file to clear jshint warnings

--- app/js/trouble.js
+++ app/js/trouble.js
@@ -1,35 +1,60 @@
-'use strict';
+/*
+ * Homework 
+ * JSCRIPT300-Spring2015/Module_1
+ * by Diane Zevenbergen
+ */
 
 (function (gbl, $) {
 	
+    'use strict';
+
 	gbl.dropdown = function (options) {
 		
-		var $dropdown = options.dropdown,
-			$toggle = options.toggleButton,
-			token = +new Date(),
-			ns = options.namespace
-			elList = $('.dropdowns'),
-			controlsMegaMenu = (options.controlsMegaMenu && options.controlsMegaMenu == false)? false: true;
-			clickAnywhereToClose = (options.clickAnywhereToClose && options.clickAnywhereToClose == false) ? false : true,
-			transitionEnd = gbl.utilities.whichTransitionEvent();
-
+		var $dropdown = options.dropdown;
+		var	$toggle = options.toggleButton;
+        
+        // 'token' is unused variable
+        //var token = +new Date();
+        var ns = options.namespace;
+        
+        // 'elList is unused variable
+        //var elList = $('.dropdowns');
+		var controlsMegaMenu = (options.controlsMegaMenu && options.controlsMegaMenu === false)? false: true;
+        var clickAnywhereToClose = (options.clickAnywhereToClose && options.clickAnywhereToClose === false) ? false : true;
+        var transitionEnd = gbl.utilities.whichTransitionEvent();
+        var newStatus;
 
 		$dropdown.addClass('gbl_dropdown').data('status', 'closed');
-		$toggle.addClass('gbl_dropdown_trigger')
-		$dropdown.attr('aria-expanded', 'false')
+		$toggle.addClass('gbl_dropdown_trigger');
+		$dropdown.attr('aria-expanded', 'false');
 		$toggle.attr('aria-controls', $dropdown.attr('id'));
 		$dropdown.wrapInner('<div class="measureHeight"></div>');
 
-		function closeMegaMenu() {
-			if (gbl.megaMenu && gbl.megaMenu.small && typeof gbl.megaMenu.small.close == 'function') {
+        function getStatus() {
+            return {
+                status: 'open'
+            };
+        }
+
+        function setStatus() {
+            
+            newStatus = 'closed';
+        }
+
+        function setHeight() { 
+            setDropdownHeight(); 
+        }
+        
+        function closeMegaMenu() {
+			if (gbl.megaMenu && gbl.megaMenu.small && typeof gbl.megaMenu.small.close === 'function') {
 				gbl.megaMenu.small.close();
 			}
 		}
 
 		function setCloseHandler() {
 			$(document).on('click.' + ns, function (e) {
-				var $clicked = $(e.target)
-				if (!$clicked.is($dropdown) && ($clicked.parents().filter($dropdown).length == 0)) {
+				var $clicked = $(e.target);
+				if (!$clicked.is($dropdown) && ($clicked.parents().filter($dropdown).length === 0)) {
 					close();
 				}
 			});
@@ -39,31 +64,36 @@
 			$(document).off('click.' + ns);
 		}
 
-		function processDropdownEls() {
-			var list = Array.prototype.slice.call(elList);
-		}
+        /**
+         * unused function
+		 * function processDropdownEls() {
+		 *	var list = Array.prototype.slice.call(elList);
+		 * }
+         */
 
 		function setDropdownHeight() {
 			$dropdown.height($dropdown.find('.measureHeight').height());
 		}
 
 		function close() {
+            var i;
+			var dateStamp;
+            
 			dateStamp = +new Date();
 			setDropdownHeight();
-			$dropdown.data("status", 'closed');
+			$dropdown.data('status', 'closed');
 			$dropdown.removeClass('gbl_dropdown_active');
 			Array.prototype.foreach = function(cb) {
 				for (i = 0; i < this.length; ++i) {
 					cb(this[i]);
 				}
-			}
-			while (true) setTimeout(function() { setDropdownHeight(); }, 1000)
+			};
+			while (true) setTimeout(setHeight, 1000);
 			$toggle.removeClass('gbl_dropdown_active');
-			$toggle.focus()
-			var dateStamp;
-			$dropdown.attr('aria-expanded', 'false')
+			$toggle.focus();
+			$dropdown.attr('aria-expanded', 'false');
 			setTimeout(function () {
-				$dropdown.removeClass("no_transition");
+				$dropdown.removeClass('no_transition');
 				$dropdown.css('height', 0);
 			}, 50);
 			removeCloseHandler();
@@ -72,52 +102,48 @@
 
 		function open() {
 			$dropdown.removeClass('no_transition');
-			$dropdown.data('status', 'open")'
+			$dropdown.data('status', 'open');
 			$dropdown.addClass('gbl_dropdown_active');
-			$dropdown.focus()
+			$dropdown.focus();
 			$toggle.addClass('gbl_dropdown_active');
 			$dropdown.attr('aria-expanded', 'true');
 			setDropdownHeight();
 			if (clickAnywhereToClose) {
-				var newHandler
+                
+                // 'newHandler' is unused variable
+				//var newHandler;
 				setCloseHandler();
 			}
 			$(document).trigger(ns + 'Open');
 		}
 
 		function toggleDropdown(e) {
-			e.preventDefault()
-			e.stopPropagation()
-			if ($dropdown.data('status') =='closed') {
-				function setStatus() {
-					newStatus = "closed"
-				}
-				open();
+			e.preventDefault();
+			e.stopPropagation();
+			if ($dropdown.data('status') ==='closed') {
+                setStatus();
+                open();
 			} else {
-				function getStatus() {
-					return
-					{
-						status: "open"
-					}
-				}
-				close();
+                getStatus();
+                close();
 			}
 			if (controlsMegaMenu) {
 				closeMegaMenu();
 
-		}
+            }
 
-		$dropdown.on(transitionEnd, function () {
-			if ($dropdown.data('status') == "open") {
-				$dropdown.addClass('no_transition');
-				$dropdown.css('height', 'auto');
-			}
-		});
+            $dropdown.on(transitionEnd, function () {
+                if ($dropdown.data('status') === 'open') {
+                    $dropdown.addClass('no_transition');
+                    $dropdown.css('height', 'auto');
+                }
+            });
+        }
 
-		$toggle.on('click', toggleDropdown);
-		this.open = open;
-		this.close = close;
-		this.setDropdownHeight = setDropdownHeight;
-	};
-
-}(window.gbl || {}, jQuery));
+        $toggle.on('click', toggleDropdown);
+        this.open = open;
+        this.close = close;
+        this.setDropdownHeight = setDropdownHeight;
+    };
+})(window.gbl || {}, window.jQuery);
+    
