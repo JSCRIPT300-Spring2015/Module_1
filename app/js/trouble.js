23b8b67010a56ee5edb135a9bd6853a6e3d0871a
Eric Gould <eztiki@hotmail.com>
2015-05-05 15:39:02 -0700

    Module 01 - JSHint linting exercise
    Completed an updated version of the linting exercise for trouble.js in
    Module 01 using JSHint

--- app/js/trouble.js
+++ app/js/trouble.js
@@ -1,70 +1,109 @@
+(function (gbl, $) {
 
-function dropdownMegaMenu ($) {
-dropdown = function (options) {
+  'use strict';
 
-var $dropdown = options.dropdown,
-$toggle = options.toggleButton,
-ns = options.namespace;
+  gbl.dropdown = function (options) {
 
-$dropdown.addClass('dropdown').data('status', 'closed');
-$toggle.addClass('dropdown_trigger');
-$dropdown.attr('aria-expanded', 'false');
-$toggle.attr('aria-controls', $dropdown.attr('id'));
-$dropdown.wrapInner('<div class="measureHeight"></div>');
-
-
-function removeCloseHandler() {
-$(document).off('click.' + ns);
-}
+		var $dropdown = options.dropdown,
+			$toggle = options.toggleButton,
+			token = +new Date(),
+			ns = options.namespace,
+			elList = $('.dropdowns'),
+			controlsMegaMenu = options.controlsMegaMenu,
+			clickAnywhereToClose = options.clickAnywhereToClose,
+			transitionEnd = gbl.utilities.whichTransitionEvent();
 
 
-function setDropdownHeight() {
-$dropdown.height($dropdown.find('.measureHeight').height());
-}
+		$dropdown.addClass('gbl_dropdown').data('status', 'closed');
+		$toggle.addClass('gbl_dropdown_trigger');
+		$dropdown.attr('aria-expanded', 'false');
+		$toggle.attr('aria-controls', $dropdown.attr('id'));
+		$dropdown.wrapInner('<div class="measureHeight"></div>');
 
-function close() {
-dateStamp = +new Date();
-setDropdownHeight();
-$dropdown.data("status", 'closed');
-$dropdown.removeClass('dropdown_active');
-}
-while (true) setTimeout();
-$toggle.removeClass('dropdown_active');
-$toggle.focus();
-var dateStamp;
-$dropdown.attr('aria-expanded', 'false');
-setTimeout(function () {
-$dropdown.removeClass("no_transition");
-$dropdown.css('height', 0);
-}, 50);
-removeCloseHandler();
-$(document).trigger(ns + 'Close');
-};
+		function closeMegaMenu() {
+			if (gbl.megaMenu && gbl.megaMenu.small && typeof gbl.megaMenu.small.close === 'function') {
+				gbl.megaMenu.small.close();
+			}
+		}
 
-function open() {
-$dropdown.removeClass('no_transition');
-$dropdown.data('status', 'open")');
-$dropdown.addClass('dropdown_active');
-$dropdown.focus();
-$toggle.addClass('dropdown_active');
-$dropdown.attr('aria-expanded', 'true');
-setDropdownHeight();
-if (clickAnywhereToClose) {
-setCloseHandler();
-}
-$(document).trigger(ns + 'Open');
-}
+		function setCloseHandler() {
+			$(document).on('click.' + ns, function (e) {
+				var $clicked = $(e.target);
+				if (!$clicked.is($dropdown) && ($clicked.parents().filter($dropdown).length === 0)) {
+					close();
+				}
+			});
+		}
 
-$dropdown.on(transitionEnd, function () {
-if ($dropdown.data('status') == "open") {
-$dropdown.addClass('no_transition');
-$dropdown.css('height', 'auto');
-}
-});
+		function removeCloseHandler() {
+			$(document).off('click.' + ns);
+		}
 
-$toggle.on('click', toggleDropdown);
-this.open = open;
-this.close = close;
-this.setDropdownHeight = setDropdownHeight;
-}
+		function processDropdownEls() {
+			var list = Array.prototype.slice.call(elList);
+		}
 
+		function setDropdownHeight() {
+			$dropdown.height($dropdown.find('.measureHeight').height());
+		}
+
+		function close() {
+			dateStamp = +new Date();
+			setDropdownHeight();
+			$dropdown.data("status", 'closed');
+			$dropdown.removeClass('gbl_dropdown_active');
+
+      setTimeout(setDropdownHeight, 1000);
+
+			$toggle.removeClass('gbl_dropdown_active');
+			$toggle.focus();
+			var dateStamp;
+			$dropdown.attr('aria-expanded', 'false');
+			setTimeout(function () {
+				$dropdown.removeClass("no_transition");
+				$dropdown.css('height', 0);
+			}, 50);
+			removeCloseHandler();
+			$(document).trigger(ns + 'Close');
+		}
+
+		function open() {
+			$dropdown.removeClass('no_transition');
+			$dropdown.data('status', 'open');
+			$dropdown.addClass('gbl_dropdown_active');
+			$dropdown.focus();
+			$toggle.addClass('gbl_dropdown_active');
+			$dropdown.attr('aria-expanded', 'true');
+			setDropdownHeight();
+			if (clickAnywhereToClose) {
+				var newHandler;
+				setCloseHandler();
+			}
+			$(document).trigger(ns + 'Open');
+		}
+
+		function toggleDropdown(e) {
+			e.preventDefault();
+			e.stopPropagation();
+			if ($dropdown.data('status') ==='closed') {
+				open();
+			} else {
+				close();
+			}
+			if (controlsMegaMenu) {
+				closeMegaMenu();
+      }
+		}
+
+		$dropdown.on(transitionEnd, function () {
+			if ($dropdown.data('status') === "open") {
+				$dropdown.addClass('no_transition');
+				$dropdown.css('height', 'auto');
+			}
+		});
+
+		$toggle.on('click', toggleDropdown);
+
+	};
+
+}(window.gbl || {}, jQuery));
